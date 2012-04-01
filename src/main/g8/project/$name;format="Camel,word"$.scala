2c624c36893c48bac464eb02583e3f102c611d0f
Chris Lewis <clewis@novus.com>
2012-04-01 14:01:22 -0400

    specs2 fix

--- src/main/g8/project/$name;format="Camel,word"$.scala
+++ src/main/g8/project/$name;format="Camel,word"$.scala
@@ -12,7 +12,7 @@ object $name;format="Camel,word"$ extends sbt.Build {
             base = file("$name$-core"),
             settings = Seq(
               libraryDependencies <++= scalaVersion (v => Seq(
-                Shared.specsDep(v) % "test"
+                Shared.specsDep(v):_*
             ))))
             
   def project(id: String, base: File, settings: Seq[Project.Setting[_]] = Nil) =
@@ -24,19 +24,23 @@ object $name;format="Camel,word"$ extends sbt.Build {
 object Shared {
   
   /** Resolve specs version for the current scala version (thanks @n8han). */
-  def specsDep(sv: String) =
-    sv.split("[.-]").toList match {
-      case "2" :: "8" :: _ => "org.scala-tools.testing" % "specs_2.8.1" % "1.6.8"
-      case "2" :: "9" :: "1" :: _ => "org.scala-tools.testing" % "specs_2.9.1" % "1.6.9"
-      case "2" :: "9" :: _ => "org.scala-tools.testing" %% "specs" % "1.6.8"
+  def specsDep(sv: String, cfg: String = "test") =
+    (sv.split("[.-]").toList match {
+      case "2" :: "8" :: "1" :: Nil =>
+        "org.specs2" %% "specs2" % "1.5" ::
+        "org.specs2" %% "specs2-scalaz-core" % "5.1-SNAPSHOT" ::
+        Nil
+      case "2" :: "9" :: "0" :: _ => "org.specs2" % "specs2_2.9.1" % "1.7.1" :: Nil
+      case "2" :: "9" :: _ :: _ => "org.specs2" % "specs2_2.9.1" % "1.8.2" :: Nil
       case _ => sys.error("Specs not supported for scala version %s" format sv)
-    }
+    }) map (_ % cfg)
   
   val settings = Seq(
     organization := "$organization$",
     version := "$version$",
     scalaVersion := "2.9.1",
     crossScalaVersions := Seq("2.8.1, 2.9.0, 2.9.0-1"),
+    resolvers += "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots",
     initialCommands := "import $organization$.$name;format="normalize,word"$._"
   )
   
