e913c7ec8e1b8743c823661401f68b06642b2b50
Chris Lewis <clewis@novus.com>
2012-04-16 11:58:54 -0400

    proper crossbuilds; scala 2.9.2; no build.sbt

--- src/main/g8/project/$name;format="Camel,word"$Build.scala
+++ src/main/g8/project/$name;format="Camel,word"$Build.scala
@@ -38,8 +38,8 @@ object Shared {
   val settings = Seq(
     organization := "$organization$",
     version := "$version$",
-    scalaVersion := "2.9.1",
-    crossScalaVersions := Seq("2.8.1, 2.9.0, 2.9.0-1"),
+    scalaVersion := "2.9.2",
+    crossScalaVersions := Seq("2.8.1", "2.9.0", "2.9.0-1", "2.9.1"),
     resolvers += "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots",
     initialCommands := "import $organization$.$name;format="normalize,word"$._"
   )
